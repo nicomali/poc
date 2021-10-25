a43bb37d8f694a0b52fefd7066e4d1d61c151279
Nico Malinovsky <nicolas.malinovsky@etermax.com>
2021-10-25 11:33:02 -0300

    initial commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+app.listen(process.env.PORT || 4000, () => console.log("Server is running"));
+
+app.get("/", (req, res) => {
+  res.status(200).send("Hello, world");
+});
